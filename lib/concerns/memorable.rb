23e6ddfa64948ee80c1688fdcd7b4feb3d6e5448
Vinn Nduri <ndurivo@gmail.com>
2022-09-01 21:27:45 +0300

    commit tree 36a343929436f0c26f8c2bec8be2695ad1b8d559

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,18 +1,18 @@
 module Memorable
 
-    module ClassMethods
-        def reset_all
-          self.all.clear
-        end
-    
-        def count
-          self.all.count
-        end
+  module ClassMethods
+      def reset_all
+        self.all.clear
       end
-
+  
+      def count
+        self.all.count
+      end
+    end
+  
     module InstanceMethods
-        def initialize
-          self.class.all << self
-        end
+      def initialize
+        self.class.all << self
       end
+    end
 end
